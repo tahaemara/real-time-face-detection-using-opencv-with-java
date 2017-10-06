daea4e7e0200163322d0f1d7105d9ea5953e21df
Taha Emara <tahaemara.eng@gmail.com>
2017-10-06 19:47:33 +0200

    Update FaceDetection.java

--- src/gui/FaceDetection.java
+++ src/gui/FaceDetection.java
@@ -1,10 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Author: Taha Emara
-//
-// Youtube Cahnnel : http://www.youtube.com/user/omar0103637
-// Facebook Page : https://www.facebook.com/IcPublishes
-// Linkedin Profile : http://eg.linkedin.com/pub/taha-emara/a4/1ab/524/
-// E-mail: : tahaemara.eng@gmail.com
+// WebSite : www.Emaraic.com
+// E-mail  : taha@emaraic.com
 //
 //                   Real time face detection using OpenCV with Java
 //
